3e5bb8d10da99815dece5bad3c94b60a4ac61827
Farhad <farhad.rakib@datasoft-bd.com>
2018-01-31 18:46:08 +0600

    details view of post added

--- posts/urls.py
+++ posts/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list,name = "post_list"),
+    url(r'^(?P<id>[0-9]+)/$', views.post_detail, name='post_details'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-
-    posts = Post.objects.filter(status = 'published')
+    posts = Post.objects.filter(status = 'published').order_by('-published')
     return render(request,'../templates/post/post_list.html',{'posts':posts})
+
+def post_detail(request,id):
+    post =get_object_or_404(Post,status = 'published' ,id=id)
+    return render(request,'../templates/post/details_post.html',{'post':post})
+
 
