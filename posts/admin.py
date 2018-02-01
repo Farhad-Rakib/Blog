ffb4245cf41275b2ef72c6ec67c027b9d8759b5c
Farhad <farhad.rakib@datasoft-bd.com>
2018-02-01 16:04:31 +0600

    filtering,searching,sorting added in admin

--- posts/admin.py
+++ posts/admin.py
@@ -2,5 +2,24 @@ from django.contrib import admin
 from .models import Category, Post
 # Register your models here.
 
-admin.site.register(Post)
-admin.site.register(Category)
+class PostModelAdmin(admin.ModelAdmin):
+    list_filter = ("title","published","created")
+    list_display = ("title","author","status","created")
+    search_fields=("title","author")
+    
+
+class CategoryModelAdmin(admin.ModelAdmin):
+    list_filter = ("name","created")
+    list_display = ("name","slug","created")
+    search_fields=("name","slug")
+
+
+
+admin.site.register(Post,PostModelAdmin)
+admin.site.register(Category,CategoryModelAdmin)
+
+
+
+### admin page header and title changed
+admin.site.site_title = 'Tech Mentor Admin'
+admin.site.site_header = 'Tech Mentor Admin'
